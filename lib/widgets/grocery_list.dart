47f1035cc1058683e1b92cb76c11b370db83d20f
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-01-04 21:30:53 +0530

    adding falback text for empty list and swipe to dismiss the list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_item.dart';
 import 'package:shopping_list/model/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -22,9 +23,37 @@ class _GroceryListState extends State<GroceryList> {
       _groceryItems.add(newItem);
     });
   }
+  void _removeItem(GroceryItem item){
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items in the list'),
+    );
+    if(_groceryItems.isNotEmpty){
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: ((ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 20,
+                  height: 20,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text(_groceryItems[index].quantity.toString()),
+              ),
+        )),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -35,18 +64,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: ((ctx, index) => ListTile(
-              title: Text(_groceryItems[index].name),
-              leading: Container(
-                width: 20,
-                height: 20,
-                color: _groceryItems[index].category.color,
-              ),
-              trailing: Text(_groceryItems[index].quantity.toString()),
-            )),
-      ),
+      body: content,
     );
   }
 }
